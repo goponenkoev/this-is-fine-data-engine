45c56e1825e9a56dacdf0a2c9608524b470a1713
Roman <v-rzhitkov@ebsco.com>
2020-10-20 16:53:45 +0300

    Add swagger

--- src/main/java/com/thisisfine/Application.java
+++ src/main/java/com/thisisfine/Application.java
@@ -1,7 +1,16 @@
 package com.thisisfine;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Data Engine Application",
+                version = "v0.1",
+                description = "API for Data Engine"
+        )
+)
 public class Application {
 
   public static void main(String[] args) {
