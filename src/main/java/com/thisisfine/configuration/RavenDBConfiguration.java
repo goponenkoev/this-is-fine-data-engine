96d16bba5531603f28573acbd058afa39188963a
Roman <rzhitkov@gmail.com>
2020-12-11 19:55:37 +0300

    Fix database creation

--- src/main/java/com/thisisfine/configuration/RavenDBConfiguration.java
+++ src/main/java/com/thisisfine/configuration/RavenDBConfiguration.java
@@ -1,20 +1,39 @@
 package com.thisisfine.configuration;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import lombok.Getter;
 import net.ravendb.client.documents.DocumentStore;
 import net.ravendb.client.documents.IDocumentStore;
+import net.ravendb.client.documents.operations.GetStatisticsOperation;
+import net.ravendb.client.exceptions.database.DatabaseDoesNotExistException;
+import net.ravendb.client.serverwide.DatabaseRecord;
+import net.ravendb.client.serverwide.operations.CreateDatabaseOperation;
+import org.apache.commons.lang3.ObjectUtils;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 @Getter
 @Singleton
 public class RavenDBConfiguration {
 
-  private final IDocumentStore store;
+    private final IDocumentStore store;
 
-  @Inject
-  public RavenDBConfiguration(DatabaseConfiguration configuration) {
-    this.store = new DocumentStore(configuration.getUrl(), configuration.getName());
-    this.store.initialize();
-  }
+    @Inject
+    public RavenDBConfiguration(DatabaseConfiguration configuration) {
+        this.store = new DocumentStore(configuration.getUrl(), configuration.getName());
+        this.store.initialize();
+        createDatabase(configuration.getName());
+    }
+
+    private void createDatabase(String database) {
+        database = ObjectUtils.firstNonNull(database, store.getDatabase());
+
+        try {
+            store.maintenance().forDatabase(database).send(new GetStatisticsOperation());
+        } catch (DatabaseDoesNotExistException e) {
+            DatabaseRecord databaseRecord = new DatabaseRecord();
+            databaseRecord.setDatabaseName(database);
+            store.maintenance().server().send(new CreateDatabaseOperation(databaseRecord));
+        }
+    }
 }
